64fd2daaf141a4023d87c2fa896020b7e5181862
GayashanSameera <gayashansameera30@gmail.com>
2021-09-24 16:25:38 +0530

    add popup

--- frontend/src/middlewares/network.js
+++ frontend/src/middlewares/network.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
-import urlMap from './config'
+import urlMap from "./config";
+import { notification } from "antd";
+
+const openNotification = () => {
+    notification.success({
+        message: "Successfully sent.",
+        className: "custom-class",
+        style: {
+            width: 400,
+        },
+    });
+};
+
+const openFailNotification = () => {
+    notification.error({
+        message: "Fail to sent.",
+        className: "custom-class",
+        style: {
+            width: 400,
+        },
+    });
+};
 
 const sendMessages = (props, callback) => {
     const { to, template, type, email } = props;
@@ -14,10 +35,12 @@ const sendMessages = (props, callback) => {
         })
         .then(function (response) {
             if (callback) callback();
+            openNotification();
             console.log(response);
         })
         .catch(function (error) {
             if (callback) callback();
+            openFailNotification();
             console.log(error);
         });
 };
